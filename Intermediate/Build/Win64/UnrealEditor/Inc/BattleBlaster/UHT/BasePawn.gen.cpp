0d07636fa3657d1a1004276045a607c97f5c42a0
Hüseyin BALTA <huso_kado@hotmail.com>
2025-12-16 08:39:38 +0300

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/BattleBlaster/UHT/BasePawn.gen.cpp
@@ -0,0 +1,152 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "BattleBlaster/BasePawn.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+static_assert(!UE_WITH_CONSTINIT_UOBJECT, "This generated code can only be compiled with !UE_WITH_CONSTINIT_OBJECT");
+void EmptyLinkFunctionForGeneratedCodeBasePawn() {}
+
+// ********** Begin Cross Module References ********************************************************
+BATTLEBLASTER_API UClass* Z_Construct_UClass_ABasePawn();
+BATTLEBLASTER_API UClass* Z_Construct_UClass_ABasePawn_NoRegister();
+ENGINE_API UClass* Z_Construct_UClass_APawn();
+ENGINE_API UClass* Z_Construct_UClass_UCapsuleComponent_NoRegister();
+ENGINE_API UClass* Z_Construct_UClass_UStaticMeshComponent_NoRegister();
+UPackage* Z_Construct_UPackage__Script_BattleBlaster();
+// ********** End Cross Module References **********************************************************
+
+// ********** Begin Class ABasePawn ****************************************************************
+FClassRegistrationInfo Z_Registration_Info_UClass_ABasePawn;
+UClass* ABasePawn::GetPrivateStaticClass()
+{
+	using TClass = ABasePawn;
+	if (!Z_Registration_Info_UClass_ABasePawn.InnerSingleton)
+	{
+		GetPrivateStaticClassBody(
+			TClass::StaticPackage(),
+			TEXT("BasePawn"),
+			Z_Registration_Info_UClass_ABasePawn.InnerSingleton,
+			StaticRegisterNativesABasePawn,
+			sizeof(TClass),
+			alignof(TClass),
+			TClass::StaticClassFlags,
+			TClass::StaticClassCastFlags(),
+			TClass::StaticConfigName(),
+			(UClass::ClassConstructorType)InternalConstructor<TClass>,
+			(UClass::ClassVTableHelperCtorCallerType)InternalVTableHelperCtorCaller<TClass>,
+			UOBJECT_CPPCLASS_STATICFUNCTIONS_FORCLASS(TClass),
+			&TClass::Super::StaticClass,
+			&TClass::WithinClass::StaticClass
+		);
+	}
+	return Z_Registration_Info_UClass_ABasePawn.InnerSingleton;
+}
+UClass* Z_Construct_UClass_ABasePawn_NoRegister()
+{
+	return ABasePawn::GetPrivateStaticClass();
+}
+struct Z_Construct_UClass_ABasePawn_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "BasePawn.h" },
+		{ "ModuleRelativePath", "BasePawn.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_CapsuleComp_MetaData[] = {
+		{ "Category", "BasePawn" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "BasePawn.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_BaseMesh_MetaData[] = {
+		{ "Category", "BasePawn" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "BasePawn.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_TurretMesh_MetaData[] = {
+		{ "Category", "BasePawn" },
+		{ "EditInline", "true" },
+		{ "ModuleRelativePath", "BasePawn.h" },
+	};
+#endif // WITH_METADATA
+
+// ********** Begin Class ABasePawn constinit property declarations ********************************
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_CapsuleComp;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_BaseMesh;
+	static const UECodeGen_Private::FObjectPropertyParams NewProp_TurretMesh;
+	static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+// ********** End Class ABasePawn constinit property declarations **********************************
+	static UObject* (*const DependentSingletons[])();
+	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<ABasePawn>::IsAbstract,
+	};
+	static const UECodeGen_Private::FClassParams ClassParams;
+}; // struct Z_Construct_UClass_ABasePawn_Statics
+
+// ********** Begin Class ABasePawn Property Definitions *******************************************
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ABasePawn_Statics::NewProp_CapsuleComp = { "CapsuleComp", nullptr, (EPropertyFlags)0x00100000000a0009, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(ABasePawn, CapsuleComp), Z_Construct_UClass_UCapsuleComponent_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_CapsuleComp_MetaData), NewProp_CapsuleComp_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ABasePawn_Statics::NewProp_BaseMesh = { "BaseMesh", nullptr, (EPropertyFlags)0x00100000000a0009, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(ABasePawn, BaseMesh), Z_Construct_UClass_UStaticMeshComponent_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_BaseMesh_MetaData), NewProp_BaseMesh_MetaData) };
+const UECodeGen_Private::FObjectPropertyParams Z_Construct_UClass_ABasePawn_Statics::NewProp_TurretMesh = { "TurretMesh", nullptr, (EPropertyFlags)0x00100000000a0009, UECodeGen_Private::EPropertyGenFlags::Object, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(ABasePawn, TurretMesh), Z_Construct_UClass_UStaticMeshComponent_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_TurretMesh_MetaData), NewProp_TurretMesh_MetaData) };
+const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_ABasePawn_Statics::PropPointers[] = {
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ABasePawn_Statics::NewProp_CapsuleComp,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ABasePawn_Statics::NewProp_BaseMesh,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_ABasePawn_Statics::NewProp_TurretMesh,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::PropPointers) < 2048);
+// ********** End Class ABasePawn Property Definitions *********************************************
+UObject* (*const Z_Construct_UClass_ABasePawn_Statics::DependentSingletons[])() = {
+	(UObject* (*)())Z_Construct_UClass_APawn,
+	(UObject* (*)())Z_Construct_UPackage__Script_BattleBlaster,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::DependentSingletons) < 16);
+const UECodeGen_Private::FClassParams Z_Construct_UClass_ABasePawn_Statics::ClassParams = {
+	&ABasePawn::StaticClass,
+	"Game",
+	&StaticCppClassTypeInfo,
+	DependentSingletons,
+	nullptr,
+	Z_Construct_UClass_ABasePawn_Statics::PropPointers,
+	nullptr,
+	UE_ARRAY_COUNT(DependentSingletons),
+	0,
+	UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::PropPointers),
+	0,
+	0x009000A4u,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams), Z_Construct_UClass_ABasePawn_Statics::Class_MetaDataParams)
+};
+void ABasePawn::StaticRegisterNativesABasePawn()
+{
+}
+UClass* Z_Construct_UClass_ABasePawn()
+{
+	if (!Z_Registration_Info_UClass_ABasePawn.OuterSingleton)
+	{
+		UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_ABasePawn.OuterSingleton, Z_Construct_UClass_ABasePawn_Statics::ClassParams);
+	}
+	return Z_Registration_Info_UClass_ABasePawn.OuterSingleton;
+}
+DEFINE_VTABLE_PTR_HELPER_CTOR_NS(, ABasePawn);
+ABasePawn::~ABasePawn() {}
+// ********** End Class ABasePawn ******************************************************************
+
+// ********** Begin Registration *******************************************************************
+struct Z_CompiledInDeferFile_FID_Users_Administrator_Documents_Unreal_Projects_Battle_Blaster_Source_BattleBlaster_BasePawn_h__Script_BattleBlaster_Statics
+{
+	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
+		{ Z_Construct_UClass_ABasePawn, ABasePawn::StaticClass, TEXT("ABasePawn"), &Z_Registration_Info_UClass_ABasePawn, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(ABasePawn), 2748210459U) },
+	};
+}; // Z_CompiledInDeferFile_FID_Users_Administrator_Documents_Unreal_Projects_Battle_Blaster_Source_BattleBlaster_BasePawn_h__Script_BattleBlaster_Statics 
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Users_Administrator_Documents_Unreal_Projects_Battle_Blaster_Source_BattleBlaster_BasePawn_h__Script_BattleBlaster_1031209079{
+	TEXT("/Script/BattleBlaster"),
+	Z_CompiledInDeferFile_FID_Users_Administrator_Documents_Unreal_Projects_Battle_Blaster_Source_BattleBlaster_BasePawn_h__Script_BattleBlaster_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Users_Administrator_Documents_Unreal_Projects_Battle_Blaster_Source_BattleBlaster_BasePawn_h__Script_BattleBlaster_Statics::ClassInfo),
+	nullptr, 0,
+	nullptr, 0,
+};
+// ********** End Registration *********************************************************************
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
